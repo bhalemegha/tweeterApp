ed6ef918a67856aa8b67925e906cb6998831b0cf
bhalemegha <meghabhale6@gmail.com>
2021-11-17 21:12:47 +0000

    Add tweet container and add flag and heart icons with hover effects

--- public/scripts/tweet-add.js
+++ public/scripts/tweet-add.js
@@ -1,41 +1,59 @@
-$(document).ready(function() {
+$(document).ready(function () {
   const tweetContainer = document.querySelector('#tweet-container');
   const textVal = "We can consider a tweet to be an article, and there's an HTML5 tag for that! You shouldn't use IDs within this component. Why not?"
-  
-  const $usrDiv = $('<div>').text("Megha");;
-  const $newUsrTweetDiv = $(tweetContainer).append($usrDiv);
-  $newUsrTweetDiv.addClass("usertweetDiv");
+  const $container = $('<div>', {
+    'class': 'tweet-txt',
+    'style' : 'border-style: solid;'
+  }).appendTo(tweetContainer);
+
+  $('<div>', {
+    'class': 'usertweetDiv',
+    'html': 'Megha'
+  }).appendTo($container);
+
+  $('<article>', {
+    'html': textVal
+  }).appendTo($container);
+
+  $iconsDivMain = $('<div>', {
+    'class': 'iconsDivMain'
+  }).appendTo($container);
+
+  $('<div>', {
+    'class': 'dateDiv',
+    'html': "10 days ago"
+  }).appendTo($iconsDivMain);
+
+  $('<div>', {
+    'html': '<i class="fas fa-flag"></i><i class="fas fa-heart"></i>'
+  }).appendTo($iconsDivMain);
+
+  // const $usrDiv = $('<div>').text("Megha");;
+  // const $newUsrTweetDiv = $(tweetContainer).append($usrDiv);
   // $($newUsrTweetDiv).css('display','flex');
   // $($newUsrTweetDiv).css('justify-content','space-around');
 
-  const $article = $("<article>").text(textVal);//It will create an HTML Article element.
-  const $newaddedElement = $(tweetContainer).append($article);//Appending newly created article element in article section
+  // const $article = $("<article>").text(textVal);//It will create an HTML Article element.
+  // const $newaddedElement = $(tweetContainer).append($article);//Appending newly created article element in article section
   // $newaddedElement.css("border-style","solid");
   // $newaddedElement.css("border-color","black");
   // $newaddedElement.css("display","flex");
   // $newaddedElement.css("justify-content","flex-start");
 
-  const $iconsDiv = $('<div>');
-  const $iconsDivMain = $(tweetContainer).append($iconsDiv);
-  $iconsDivMain.addClass("iconsDivMain");
+  // const $iconsDiv = $('<div>');
+  // const $iconsDivMain = $(tweetContainer).append($iconsDiv);
   // $iconsDivMain.css('display','flex');
   // $iconsDivMain.css('flex-direction','row');
   // $iconsDivMain.css('justify-content','space-around');
 
-  const $divTime = $('<div>').text("timer");
-  const $dateDiv = $iconsDivMain.append($divTime);
-  $dateDiv.addClass("dateDiv");
+  // const $divTime = $('<div>').text("timer");
+  // const $dateDiv = $iconsDivMain.append($divTime);
   // $dateDiv.css("display","flex");
   // $dateDiv.css("width","80%");
   // $dateDiv.css("justify-content", "flex-start");
 
-  const $divIcons = $('<div>').text("icons");
-  $iconsDivMain.append($divIcons);
+  // const $divIcons = $('<div>').text("icons");
+  // $iconsDivMain.append($divIcons);
 
 });
 
-
-
-// display: flex;
-// flex-direction: row;
-// justify-content: space-around;
