656d04b7ef02399855e8c1d0f02edfeb4657d0f0
bhalemegha <meghabhale6@gmail.com>
2021-11-18 23:45:53 +0000

    Add media query to make UI responsive

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -3,15 +3,15 @@
  * jQuery is already loaded
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
-$(document).ready(function () {
+$(document).ready(function() {
 
-  const escape = function (str) {
+  const escape = function(str) {
     let div = document.createElement("div");
     div.appendChild(document.createTextNode(str));
     return div.innerHTML;
   };
 
-  const createTweetElement = function (tweetData) {
+  const createTweetElement = function(tweetData) {
     const usrName = tweetData["user"]["name"];
     const handle = tweetData["user"]["handle"];
     const content = tweetData["content"]["text"];
@@ -23,15 +23,17 @@ $(document).ready(function () {
     const $tweetHandle = $('<div>', {
       'class': 'usertweetDiv'
     }).appendTo($container);
-    //it will show user name
+
+    //To add Avataar
     const $img = $('<img>', {
       'src': escape(avataar)
     }).appendTo($tweetHandle);
 
+    //To show user name
     $('<div>', {
       'class': 'dateDiv',
       'html': escape(usrName)
-    }).appendTo($img);
+    }).appendTo($tweetHandle);
 
     //for handle
     $('<div>', {
@@ -44,7 +46,7 @@ $(document).ready(function () {
     }).appendTo($container);
 
     //container to hold time ago and various icon like flag, heart and sync
-    $iconsDivMain = $('<div>', {
+    const $iconsDivMain = $('<div>', {
       'class': 'iconsDivMain'
     }).appendTo($container);
 
@@ -54,62 +56,71 @@ $(document).ready(function () {
       'html': escape(createdAt)
     }).appendTo($iconsDivMain);
 
-    //icons container
+    //flag, heart and sync icons container
     $('<div>', {
       'html': '<i class="fas fa-flag"></i><i class="fa fa-refresh" aria-hidden="true"></i><i class="fas fa-heart"></i>'
     }).appendTo($iconsDivMain);
     return $container;
-  }
+  };
 
+  //clears text area and reset counter once tweet has been posted
   const clearTextArea = function() {
     $('textarea').val("");
+    $('#errorDiv').html("");
     $('div output').text(140);
-  }
+  };
 
   //To render tweets data, iterating through all tweets, creating element for it and append it section
-  const renderTweets = function (tweets) {
+  const renderTweets = function(tweets) {
     for (let tweet of tweets) {
-      const $tweetEle = createTweetElement(tweet)
+      const $tweetEle = createTweetElement(tweet);
       $('#tweet-container').append($tweetEle);
     }
     clearTextArea();
-  }
+  };
 
   //renderTweets(data);
 
-  $( "form" ).on( "submit", function( event ) {
+  $("form").on("submit", function(event) {
+    //To prevent default behaviour of browser once form has been submitted.
     event.preventDefault();
+    
+    //checks for empty string
     let tweetData = $('textarea').first().val();
-    if (!tweetData) {//if no text
+    if (!tweetData) {
       $('#errorDiv').text("Tweet text should not be blank!");
       $('#errorDiv').show();
       return;
     }
-    if (tweetData.length >= 140) {//check it it's too
+    //check it it's longer than limit(140 for now)
+    if (tweetData.length > 140) {
       $('#errorDiv').text("Tweet text is too long!!");
       $('#errorDiv').show();
       return;
     }
-
-    let tweetUrl = $( this ).attr('action');//getting form action
-    tweetData = $(this).serialize();//adding form data to query string
-    $.ajax({//sending post request
+    //getting form action
+    let tweetUrl = $(this).attr('action');
+    //adding form data to query string
+    tweetData = $(this).serialize();
+    //sending post request
+    $.ajax({
       type: "POST",
       url: tweetUrl,
       data: tweetData,
+      //renders tweets on success
       success:loadTweets()
     });
   });
 
+  //fetching tweets
   const loadTweets = function() {
     const url = "/tweets/";
     $.ajax({
       url: url,
       method: "GET"
     })
-    .then((data) => {
-      renderTweets(data);
-    }) 
-  }
- 
+      .then((data) => {
+        renderTweets(data);
+      });
+  };
 });
--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,18 +1,20 @@
 $(document).ready(function() {
 
   //checks number of chars on key up events and update max char limit.
-  const txtArea = document.querySelector('form textarea');
+  // const txtArea = document.querySelector('form textarea');
   const counterDiv = document.querySelector('div output');
   const maxCounterLen = $(counterDiv).val();//140 for now,
-  $('textarea').keyup(function (event){
+  $('textarea').keyup(function() {
     $('#errorDiv').hide();
     const inputTextLength = $(this).val().length;
     if (inputTextLength > maxCounterLen) {
-      $(counterDiv).css('color','red'); //max char turn in red color if text exceeds the limit 140(maxcounterLen)
-    } 
+      //max char turn in red color if text exceeds the limit 140(maxcounterLen)
+      $(counterDiv).css('color','red');
+    }
     if (inputTextLength <= maxCounterLen) {
       $(counterDiv).css('color','black');
     }
-    $(counterDiv).html( maxCounterLen - inputTextLength);//set new val for max length display once user starts typing on text box
+    //set new val for max length display once user starts typing on text box
+    $(counterDiv).html(maxCounterLen - inputTextLength);
   });
 });
